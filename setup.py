2dffbe9ebb0c9970e5f01f541b9e84219e8e72ea
chawkk6404 <chawk_k@outlook.com>
2022-12-04 12:27:10 -0800

    Fix string

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ setup(
         'Intended Audience :: Developers',
         'Natural Language :: English',
         'Operating System :: OS Independent',
-        'Programming Language :: Python :: 3.7'
+        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
