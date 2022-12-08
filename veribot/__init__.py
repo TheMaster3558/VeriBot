44d67521a065c1ee30e5d23bccb705ed2bb5a0ea
chawkk6404 <chawk_k@outlook.com>
2022-12-07 17:46:43 -0800

    Bump version

--- veribot/__init__.py
+++ veribot/__init__.py
@@ -10,7 +10,7 @@ __title__: Final[str] = 'veribot'
 __author__: Final[str] = 'The Master'
 __license__: Final[str] = 'MIT'
 __copyright__: Final[str] = 'Copyright 2022-present The Master'
-__version__: Final[str] = '1.0.5'
+__version__: Final[str] = '1.1.0'
 
 
 from .bot import VeriBot
